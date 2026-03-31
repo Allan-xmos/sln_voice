8ebf29f747e690ebf6c38549a2908eab4e95485c
Allan Skellett <allanskellett@xmos.com>
2026-03-31 14:35:43 +0100

    Update copyright dates

--- examples/ffva/src/control/servicer.c
+++ examples/ffva/src/control/servicer.c
@@ -1,4 +1,4 @@
-// Copyright 2024 XMOS LIMITED.
+// Copyright 2024-2026 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 #define DEBUG_UNIT SERVICER_TASK
 #ifndef DEBUG_PRINT_ENABLE_SERVICER_TASK
